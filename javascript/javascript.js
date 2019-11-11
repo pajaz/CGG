272f16235f34c2dd19c95acd792c3dc24c6c52ad
pajaz <32343973+pajaz@users.noreply.github.com>
2019-11-11 12:21:24 +0700

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,10 +2,10 @@
 function getContent(page) {
     var cnt = page + ".html"
 
-    console.log("Working")
+    console.log(cnt)
     $(document).ready(function() {
         console.log("Working2")
-        $('#content').load(page);
+        $('#content').load(cnt);
 
     });
 }
