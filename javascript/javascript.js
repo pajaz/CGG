f093b8010866db72573e65616e059401547c4f5a
pajaz <32343973+pajaz@users.noreply.github.com>
2019-11-14 14:17:41 +0700

    Styling menupages

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,11 +1,51 @@
+var state = "none"
 
-function getContent(page) {
+function getContent(page, divid, menu) {
     var cnt = page + ".txt"
+        id = "#content" + divid
+    
+    if (divid=="2") {
+        console.log(menu + " Onko olemassa")
+        setButtonFocus(page)
+    }
 
-    console.log(cnt)
+    console.log(id)
     $(document).ready(function() {
-        console.log("Working2")
-        $('#content').load(cnt);
+        $(id).load(cnt);
 
     });
+    
+    if (menu !== undefined) {
+            state = menu
+        } 
 }
+
+function checkState() {
+    if (state == "none") {
+        return
+    } 
+    console.log(state)
+    getContent(state, "2")
+
+    state = "none"
+
+}
+
+function setButtonFocus(bb) {
+    var b = bb
+
+    console.log(b + " TEST")
+    switch (b) {
+        case "alacartePage":
+            document.getElementById("alacarte").focus()
+            break;
+        case "lunchPage":
+            document.getElementById("lunch").focus()
+            break;
+        case "drinkPage":
+            document.getElementById("drink").focus()
+            break;
+
+    }
+}
+
