5b49595911dd265b9391e16bfd3d6e5e6b007309
pajaz <32343973+pajaz@users.noreply.github.com>
2019-11-14 15:12:33 +0700

    rolling backgrounds

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,7 +12,6 @@ function getContent(page, divid, menu) {
     console.log(id)
     $(document).ready(function() {
         $(id).load(cnt);
-
     });
     
     if (menu !== undefined) {
@@ -49,3 +48,23 @@ function setButtonFocus(bb) {
     }
 }
 
+function setBackground() {
+    $(function() {
+        var body = $(".bg-mid");
+        var backgrounds = [
+          'url(../resources/background1.jpg)', 
+          'url(../resources/background2.jpg)'];
+          console.log(backgrounds)
+        var current = 0;
+        function nextBackground() {
+            body.css(
+                'background',
+            backgrounds[current = ++current % backgrounds.length]);
+    
+            setTimeout(nextBackground, 10000);
+        }
+        setTimeout(nextBackground, 10000);
+        body.css('background', backgrounds[0]);
+    });
+
+}
